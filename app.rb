6b580d15d63baf0c9ed42faf51c2303b63505819
Erin Zobitz <erinzobitz@gmail.com>
2012-12-03 23:07:52 -0800

    html whois lookup

---
+++ app.rb
@@ -0,0 +1,23 @@
+require "sinatra"
+require "whois"
+require "sinatra/reloader"
+
+set :public_folder, File.dirname(__FILE__) + '/public'
+
+get '/' do
+  erb :index
+end
+
+post '/whois' do
+	puts params
+	w = Whois::Client.new
+	@state = "unavailable"
+	@domain = params[:domain]
+	result = w.query(@domain)
+
+	@state = "available" if result.available?
+
+	erb :result
+end
+
+
